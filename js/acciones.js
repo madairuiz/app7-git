10b1736a217db66fd5e3afd48c8160d9e64e58d8
madairuiz <38224781+madairuiz@users.noreply.github.com>
2018-05-22 08:57:28 -0500

    subida2

--- js/acciones.js
+++ js/acciones.js
@@ -1,22 +1,23 @@
 // JavaScript Document
-$(document).ready(function(e){
-	document.addEventListener("deviceready", function(){
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function(){
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizó a la izquierda",function(){"aplicación7","Aceptar"}); 
-		});//cierre deslizar izquierda
-		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Qué quieres hacer? ",function(opt){
-				switch(opt)
-				{
-					case 1:
-					navigator.notification.beep(2);
-					break;
-					
-					case 2:
-					navigator.notification.vibrate(2000);
-					break;
-				}
-			},"Aplicación7","Beepear,Vibrar,Cancelar");
-		});//Cierre de deslizar derecha
-	}, false);//Cierre del deviceready 
-});//Cierre del documento principal
+			navigator.notification.alert("Deslizó a la izquierda",function(){"aplicación7","Aceptar"});
+			}); //cierre deslizar izquierda
+			$('#derecha').on("swiperight",function(){
+				navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+					switch(opt)
+					{
+						case 1:
+						navigator.notification.beep(2);
+						break;
+						
+						case 2:
+						navigator.notification.Vibrate(2000);
+						break;
+					}
+				},"aplicación7","Beepear,Vibrar,Cancelar");
+			});// cierre deslizar derecha
+	},false); // cierre del deviceready
+}); // cierre del document principal
+
